1da3180c3eafb5d2fe4120296697b94bff27615e
Christian Ihle <blurpy@gmail.com>
2021-04-02 13:04:11 +0200

    Fix consistency in asm formatting

--- programs/add_two_numbers.asm
+++ programs/add_two_numbers.asm
@@ -1,6 +1,6 @@
 LDA 14     ; Put the value from memory location 14 in the A-register
 ADD 15     ; Put the value from memory location 15 in the B-register, and store A+B in the A-register
-OUT        ; Output the value from the A-register
+OUT        ; Output the value of the A-register
 HLT        ; Halt the computer
 ORG 14     ; Change memory location to 14
 DB  28     ; Define a byte with the value 28 at memory location 14
--- programs/increase_by_three.asm
+++ programs/increase_by_three.asm
@@ -1,6 +1,6 @@
-LDI 3      ; Put the value 3 in the A-register
-STA 15     ; Store the value 3 from the A-register in memory location 15
-LDI 0      ; Put the value 0 in the A-register
+LDI  3     ; Put the value 3 in the A-register
+STA 15     ; Store the value from the A-register in memory location 15
+LDI  0     ; Put the value 0 in the A-register
 ADD 15     ; Put the value from memory location 15 in the B-register, and store A+B in the A-register
 OUT        ; Output the value of the A-register
-JMP 3      ; Jump to instruction 3
+JMP  3     ; Jump to instruction 3
