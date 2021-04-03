12746af096a2feb9bf980e522d6f740e1cd0b580
Christian Ihle <blurpy@gmail.com>
2021-04-03 13:40:04 +0200

    Add short description of the instructions

--- src/Instructions.h
+++ src/Instructions.h
@@ -21,17 +21,29 @@ public:
         constexpr bool operator!=(Instruction id) const { return opcode == id.opcode && mnemonic == id.mnemonic; }
     };
 
+    /** No operation */
     static constexpr Instruction NOP = {"NOP", 0b0000, false};
+    /** Load the accumulator */
     static constexpr Instruction LDA = {"LDA", 0b0001, true};
+    /** Add */
     static constexpr Instruction ADD = {"ADD", 0b0010, true};
+    /** Subtract */
     static constexpr Instruction SUB = {"SUB", 0b0011, true};
+    /** Store the accumulator */
     static constexpr Instruction STA = {"STA", 0b0100, true};
+    /** Load immediate */
     static constexpr Instruction LDI = {"LDI", 0b0101, true};
+    /** Jump */
     static constexpr Instruction JMP = {"JMP", 0b0110, true};
+    /** Jump if carry */
     static constexpr Instruction JC  = {"JC",  0b0111, true};
+    /** Jump if zero */
     static constexpr Instruction JZ  = {"JZ",  0b1000, true};
+    /** Output value */
     static constexpr Instruction OUT = {"OUT", 0b1110, false};
+    /** Halt the computer */
     static constexpr Instruction HLT = {"HLT", 0b1111, false};
+    /** Unknown instruction */
     static constexpr Instruction UNKNOWN = {"UNKNOWN", 0, false};
 
     static Instruction find(const std::string& mnemonic);
