d094161fdd4ade133c1e5a296e7af87a1050b887
Christian Ihle <blurpy@gmail.com>
2021-04-05 12:42:36 +0200

    Add test of StepCounter

---
+++ test/StepCounterTest.cpp
@@ -0,0 +1,58 @@
+#include "doctest.h"
+#include "fakeit.hpp"
+
+#include "StepCounter.h"
+
+TEST_SUITE("StepCounterTest") {
+    TEST_CASE("inverted clock ticks should notify listener of incrementing steps and wrap at 4") {
+        fakeit::Mock<StepListener> stepListenerMock;
+        auto stepListenerMockSharedPtr = std::shared_ptr<StepListener>(&stepListenerMock(), [](...) {});
+        StepCounter stepCounter(stepListenerMockSharedPtr);
+
+        fakeit::When(Method(stepListenerMock, stepReady)).AlwaysReturn();
+
+        auto &clock = dynamic_cast<ClockListener&>(stepCounter);
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(0)).Once();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(1)).Once();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(2)).Once();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(3)).Once();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(4)).Once();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(0)).Twice();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(1)).Twice();
+    }
+
+    TEST_CASE("reset() should start counting steps at 0 again") {
+        fakeit::Mock<StepListener> stepListenerMock;
+        auto stepListenerMockSharedPtr = std::shared_ptr<StepListener>(&stepListenerMock(), [](...) {});
+        StepCounter stepCounter(stepListenerMockSharedPtr);
+
+        fakeit::When(Method(stepListenerMock, stepReady)).AlwaysReturn();
+
+        auto &clock = dynamic_cast<ClockListener&>(stepCounter);
+
+        clock.invertedClockTicked();
+        clock.invertedClockTicked();
+
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(0)).Once();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(1)).Once();
+
+        stepCounter.reset();
+
+        clock.invertedClockTicked();
+        fakeit::Verify(Method(stepListenerMock, stepReady).Using(0)).Twice();
+    }
+}
