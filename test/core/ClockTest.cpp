1ca80e884e548ba2280191e852722ed98aa7a536
Christian Ihle <blurpy@gmail.com>
2021-04-17 18:55:41 +0200

    Changing to subcase in clock test

--- test/core/ClockTest.cpp
+++ test/core/ClockTest.cpp
@@ -11,7 +11,7 @@ double milliToNano(double milliseconds) {
 }
 
 TEST_SUITE("ClockTest") {
-    TEST_CASE("singleStep() should notify listener of falling and rising edge in the correct time") {
+    TEST_CASE("clock should work correctly") {
         fakeit::Mock<TimeSource> timeSourceMock;
         auto timeSourcePtr = std::shared_ptr<TimeSource>(&timeSourceMock(), [](...) {});
         fakeit::Mock<ClockListener> listenerMock;
@@ -26,18 +26,20 @@ TEST_SUITE("ClockTest") {
         fakeit::When(Method(listenerMock, clockTicked)).AlwaysReturn();
         fakeit::When(Method(listenerMock, invertedClockTicked)).AlwaysReturn();
 
-        clock.setFrequency(1);
-        clock.singleStep();
+        SUBCASE("singleStep() should notify listener of falling and rising edge in the correct time") {
+            clock.setFrequency(1);
+            clock.singleStep();
 
-        // 1 Hz = 1 clock cycle per second
-        // 1 cycle is 2 completed ticks - 1 rising edge (clock tick) and 1 falling edge (inverted clock tick)
-        // 1 time delta takes 100 ms (0.1s)
-        // To spend 1 second on this single step, we have to ask for delta and sleep 10 times
+            // 1 Hz = 1 clock cycle per second
+            // 1 cycle is 2 completed ticks - 1 rising edge (clock tick) and 1 falling edge (inverted clock tick)
+            // 1 time delta takes 100 ms (0.1s)
+            // To spend 1 second on this single step, we have to ask for delta and sleep 10 times
 
-        // Clock tick must be the first, and then inverted clock tick
-        fakeit::Verify(Method(listenerMock, clockTicked), Method(listenerMock, invertedClockTicked)).Once();
-        fakeit::Verify(Method(timeSourceMock, delta)).Exactly(10);
-        fakeit::Verify(Method(timeSourceMock, sleep)).Exactly(10);
-        fakeit::Verify(Method(timeSourceMock, reset)).Once();
+            // Clock tick must be the first, and then inverted clock tick
+            fakeit::Verify(Method(listenerMock, clockTicked), Method(listenerMock, invertedClockTicked)).Once();
+            fakeit::Verify(Method(timeSourceMock, delta)).Exactly(10);
+            fakeit::Verify(Method(timeSourceMock, sleep)).Exactly(10);
+            fakeit::Verify(Method(timeSourceMock, reset)).Once();
+        }
     }
 }
