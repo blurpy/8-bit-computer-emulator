987e0adb726f3074bad4d6e79dce8a38b690f3a8
Christian Ihle <blurpy@gmail.com>
2021-04-22 19:17:54 +0200

    Convert some test cases to subcases in FlagsRegisterTest to avoid boilerplate

--- test/core/FlagsRegisterTest.cpp
+++ test/core/FlagsRegisterTest.cpp
@@ -6,65 +6,53 @@
 using namespace Core;
 
 TEST_SUITE("FlagsRegisterTest") {
-    TEST_CASE("flags should be false on new instance") {
+    TEST_CASE("flags register should work correctly") {
         fakeit::Mock<ArithmeticLogicUnit> aluMock;
         auto aluMockSharedPtr = std::shared_ptr<ArithmeticLogicUnit>(&aluMock(), [](...) {});
         FlagsRegister flagsRegister(aluMockSharedPtr);
-
-        CHECK_FALSE(flagsRegister.isCarryFlag());
-        CHECK_FALSE(flagsRegister.isZeroFlag());
-    }
-
-    TEST_CASE("clockTicked() should not update flags without in()") {
-        fakeit::Mock<ArithmeticLogicUnit> aluMock;
-        auto aluMockSharedPtr = std::shared_ptr<ArithmeticLogicUnit>(&aluMock(), [](...) {});
-        FlagsRegister flagsRegister(aluMockSharedPtr);
-
-        fakeit::When(Method(aluMock, isCarry)).Return(true);
-        fakeit::When(Method(aluMock, isZero)).Return(true);
-
-        CHECK_FALSE(flagsRegister.isCarryFlag());
-        CHECK_FALSE(flagsRegister.isZeroFlag());
 
         auto &clock = dynamic_cast<ClockListener&>(flagsRegister);
-        clock.clockTicked();
-
-        CHECK_FALSE(flagsRegister.isCarryFlag());
-        CHECK_FALSE(flagsRegister.isZeroFlag());
-    }
-
-    TEST_CASE("clockTicked() should update flags after in()") {
-        fakeit::Mock<ArithmeticLogicUnit> aluMock;
-        auto aluMockSharedPtr = std::shared_ptr<ArithmeticLogicUnit>(&aluMock(), [](...) {});
-        FlagsRegister flagsRegister(aluMockSharedPtr);
-
-        fakeit::When(Method(aluMock, isCarry)).Return(true);
-        fakeit::When(Method(aluMock, isZero)).Return(true);
-
-        flagsRegister.in();
 
         CHECK_FALSE(flagsRegister.isCarryFlag());
         CHECK_FALSE(flagsRegister.isZeroFlag());
 
-        auto &clock = dynamic_cast<ClockListener&>(flagsRegister);
-        clock.clockTicked();
+        SUBCASE("clockTicked() should not update flags without in()") {
+            fakeit::When(Method(aluMock, isCarry)).Return(true);
+            fakeit::When(Method(aluMock, isZero)).Return(true);
 
-        CHECK(flagsRegister.isCarryFlag());
-        CHECK(flagsRegister.isZeroFlag());
+            CHECK_FALSE(flagsRegister.isCarryFlag());
+            CHECK_FALSE(flagsRegister.isZeroFlag());
 
-        SUBCASE("reset() should set flags to false") {
-            flagsRegister.reset();
+            clock.clockTicked();
 
             CHECK_FALSE(flagsRegister.isCarryFlag());
             CHECK_FALSE(flagsRegister.isZeroFlag());
         }
-    }
 
-    TEST_CASE("print() should not fail") {
-        fakeit::Mock<ArithmeticLogicUnit> aluMock;
-        auto aluMockSharedPtr = std::shared_ptr<ArithmeticLogicUnit>(&aluMock(), [](...) {});
-        FlagsRegister flagsRegister(aluMockSharedPtr);
+        SUBCASE("clockTicked() should update flags after in()") {
+            fakeit::When(Method(aluMock, isCarry)).Return(true);
+            fakeit::When(Method(aluMock, isZero)).Return(true);
 
-        flagsRegister.print();
+            flagsRegister.in();
+
+            CHECK_FALSE(flagsRegister.isCarryFlag());
+            CHECK_FALSE(flagsRegister.isZeroFlag());
+
+            clock.clockTicked();
+
+            CHECK(flagsRegister.isCarryFlag());
+            CHECK(flagsRegister.isZeroFlag());
+
+            SUBCASE("reset() should set flags to false") {
+                flagsRegister.reset();
+
+                CHECK_FALSE(flagsRegister.isCarryFlag());
+                CHECK_FALSE(flagsRegister.isZeroFlag());
+            }
+        }
+
+        SUBCASE("print() should not fail") {
+            flagsRegister.print();
+        }
     }
 }
