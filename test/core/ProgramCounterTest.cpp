f177a6dd8cb08e153d52a2f5ce1257fa1c93f3a4
Christian Ihle <blurpy@gmail.com>
2021-05-14 15:29:51 +0200

    Convert ProgramCounterTest to subcases

--- test/core/ProgramCounterTest.cpp
+++ test/core/ProgramCounterTest.cpp
@@ -5,157 +5,123 @@
 using namespace Core;
 
 TEST_SUITE("ProgramCounterTest") {
-    TEST_CASE("out() should put 0 on the bus on new instance") {
+    TEST_CASE("program counter should work") {
         const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
         ProgramCounter programCounter(busSharedPtr);
 
-        programCounter.out();
+        auto &clock = dynamic_cast<ClockListener&>(programCounter);
 
+        programCounter.out();
         CHECK_EQ(busSharedPtr->read(), 0);
-    }
 
-    TEST_CASE("clock tick should do nothing by itself") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
+        SUBCASE("clock tick should do nothing by itself") {
+            busSharedPtr->write(8);
 
-        busSharedPtr->write(8);
-
-        clock.clockTicked();
-
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 0); // No jump, and no increment
-    }
-
-    TEST_CASE("enable() should increment on clock tick") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
-
-        programCounter.enable();
-
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 0); // Nothing happened yet
-
-        clock.clockTicked();
-
-        CHECK_EQ(busSharedPtr->read(), 0);
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 1);
-    }
-
-    TEST_CASE("enable() should only increment on first clock tick") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
-
-        programCounter.enable();
-        clock.clockTicked();
-
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 1);
-
-        clock.clockTicked();
-
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 1); // No change
-    }
-
-    TEST_CASE("enable() should wrap around") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
-
-        for (int i = 0; i < 15; i++) {
-            programCounter.enable();
             clock.clockTicked();
+
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 0); // No jump, and no increment
         }
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 15);
+        SUBCASE("enable() should increment on clock tick") {
+            programCounter.enable();
 
-        programCounter.enable();
-        clock.clockTicked();
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 0); // Nothing happened yet
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 0);
-    }
+            clock.clockTicked();
 
-    TEST_CASE("jump() should change counter to value from bus on clock tick") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
+            CHECK_EQ(busSharedPtr->read(), 0);
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 1);
+        }
 
-        busSharedPtr->write(8);
+        SUBCASE("enable() should only increment on first clock tick") {
+            programCounter.enable();
+            clock.clockTicked();
 
-        programCounter.jump();
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 1);
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 0); // Nothing read from the bus yet
+            clock.clockTicked();
 
-        busSharedPtr->write(6); // This should be stored
-        clock.clockTicked();
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 1); // No change
+        }
 
-        busSharedPtr->write(4); // Change the bus after storing the last value, otherwise it's difficult to know
+        SUBCASE("enable() should wrap around") {
+            for (int i = 0; i < 15; i++) {
+                programCounter.enable();
+                clock.clockTicked();
+            }
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 6);
-    }
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 15);
 
-    TEST_CASE("jump() should only change on first clock tick") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
+            programCounter.enable();
+            clock.clockTicked();
 
-        busSharedPtr->write(15);
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 0);
+        }
 
-        programCounter.jump();
-        clock.clockTicked();
+        SUBCASE("jump() should change counter to value from bus on clock tick") {
+            busSharedPtr->write(8);
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 15);
+            programCounter.jump();
 
-        busSharedPtr->write(6);
-        clock.clockTicked();
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 0); // Nothing read from the bus yet
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 15); // No change
-    }
+            busSharedPtr->write(6); // This should be stored
+            clock.clockTicked();
 
-    TEST_CASE("jump() should throw exception on clock tick if value more than 4 bits") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
+            busSharedPtr->write(4); // Change the bus after storing the last value, otherwise it's difficult to know
 
-        busSharedPtr->write(16);
-        programCounter.jump();
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 6);
+        }
 
-        CHECK_THROWS_WITH(clock.clockTicked(), "ProgramCounter: address out of bounds 16");
-    }
+        SUBCASE("jump() should only change on first clock tick") {
+            busSharedPtr->write(15);
 
-    TEST_CASE("reset() should start counting at 0 again") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
-        auto &clock = dynamic_cast<ClockListener &>(programCounter);
+            programCounter.jump();
+            clock.clockTicked();
 
-        busSharedPtr->write(8);
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 15);
 
-        programCounter.jump();
-        clock.clockTicked();
+            busSharedPtr->write(6);
+            clock.clockTicked();
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 8);
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 15); // No change
+        }
 
-        programCounter.reset();
+        SUBCASE("jump() should throw exception on clock tick if value more than 4 bits") {
+            busSharedPtr->write(16);
+            programCounter.jump();
 
-        programCounter.out();
-        CHECK_EQ(busSharedPtr->read(), 0);
-    }
+            CHECK_THROWS_WITH(clock.clockTicked(), "ProgramCounter: address out of bounds 16");
+        }
 
-    TEST_CASE("print() should not fail") {
-        const std::shared_ptr<Bus> &busSharedPtr = std::make_shared<Bus>();
-        ProgramCounter programCounter(busSharedPtr);
+        SUBCASE("reset() should start counting at 0 again") {
+            busSharedPtr->write(8);
 
-        programCounter.print();
+            programCounter.jump();
+            clock.clockTicked();
+
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 8);
+
+            programCounter.reset();
+
+            programCounter.out();
+            CHECK_EQ(busSharedPtr->read(), 0);
+        }
+
+        SUBCASE("print() should not fail") {
+            programCounter.print();
+        }
     }
 }
